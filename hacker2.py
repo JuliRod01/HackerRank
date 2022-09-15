07d4c9a9eb849956ee216ba4fb4ab1a2b7fb24bd
JuliRod01 <julianarodriguez01@gmail.com>
2022-09-15 14:52:12 -0500

    Second hacker rank problem 3-month kit

---
+++ hacker2.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    min =0
+    max=0
+    arr = sorted(arr)
+    for i in range(0,(len(arr)-1)):
+        min += arr[i]
+    for i in range(1, len(arr)):
+        max += arr[i]
+    print(f'{min} {max}')
+        
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
+
